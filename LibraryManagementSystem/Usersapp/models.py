1abf2b950b20978827dfd7510e040367f11079da
Pushpak Chaudhari <pushpakb.chaudhari@gmail.com>
2021-11-12 01:25:14 +0530

    Second Commit

--- LibraryManagementSystem/Usersapp/models.py
+++ LibraryManagementSystem/Usersapp/models.py
@@ -1,15 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
 
 
 # Create your models here.
 alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
-class User(models.Model):
+class User(AbstractUser):
 	first_name = models.CharField(max_length=100, default='')
 	last_name = models.CharField(max_length=100, default='')
 	email = models.EmailField(max_length=100, default='', unique=True)
 	password1 = models.CharField(max_length=16, default='0',validators=[alphanumeric])
 	password2 = models.CharField(max_length=16, default='0',validators=[alphanumeric])
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = []
 
 	def __str__(self):
 		return str(self.email)
--- LibraryManagementSystem/Usersapp/views.py
+++ LibraryManagementSystem/Usersapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .forms import SignUpForm
@@ -10,7 +12,24 @@ def login(request):
 
 def signup(request):
 	SignUp = SignUpForm
-	return render(request, 'user/signup.html', {'SignUpForm': SignUp})
+	if request.method == 'POST':
+		first_name = request.POST['first_name']
+		last_name = request.POST['last_name']
+		email = request.POST['email']
+		password1 = request.POST['password1']
+		password2 = request.POST['password2']
+		if password1==password2:
+			if User.objects.filter(email=email).exists():
+				messages.info(request,'Username Taken')
+			else:
+				user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password1)
+				user.save()
+				messages.info(request, 'User Created')
+		else:
+			messages.info(request, 'User Created')
+		return redirect('/')
+	else:
+		return render(request, 'user/signup.html', {'SignUpForm': SignUp})
 
 
 def logout(request):
