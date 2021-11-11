31d9d927dd2ba7a8b8e67e12be03b180462ce933
Pushpak Chaudhari <pushpakb.chaudhari@gmail.com>
2021-11-12 00:42:23 +0530

    Add git ignore

--- LibraryManagementSystem/Usersapp/forms.py
+++ LibraryManagementSystem/Usersapp/forms.py
@@ -14,12 +14,12 @@ x7 = forms.TextInput(attrs={'class': "form-control"})
 # Sign Up Forms
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True, widget=x3, label=False)
-    last_name = forms.CharField(max_length=30, required=True, widget=x4, label=False)
-    email = forms.EmailField(max_length=254, required=True, widget=x6, label=False)
-    password1 = forms.CharField(required=True, widget=x1, label=False)
-    password2 = forms.CharField(required=True, widget=x2, label=False)
+	first_name = forms.CharField(max_length=30, required=True, widget=x3, label=False)
+	last_name = forms.CharField(max_length=30, required=True, widget=x4, label=False)
+	email = forms.EmailField(max_length=254, required=True, widget=x6, label=False)
+	password1 = forms.CharField(required=True, widget=x1, label=False)
+	password2 = forms.CharField(required=True, widget=x2, label=False)
 
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+	class Meta(UserCreationForm.Meta):
+		model = User
+		fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
